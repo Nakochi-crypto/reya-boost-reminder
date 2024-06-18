b2e3aecd0f963685b95d9e7aece30c9432abc22c
nakochi <nakochi@kudasaijp.com>
2024-06-18 20:29:26 +0900

    Update

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,19 +1,22 @@
 import { z } from 'zod';
 
-const RenaApiResponseSchema = z.object({
+const ReyaApiResponseSchema = z.object({
   bodyId: z.string(),
   status: z.enum(['lockedIn', 'expired', 'notStarted', 'notLockedIn']),
   boostRate: z.number(),
   nextBoostStartTimestampMilliseconds: z.number(),
 });
 
-type RenaApiResponse = z.infer<typeof RenaApiResponseSchema>;
+type ReyaApiResponse = z.infer<typeof ReyaApiResponseSchema>;
 
 export async function fetchExpiration(
   address: string,
-): Promise<RenaApiResponse> {
+): Promise<ReyaApiResponse> {
   const url = `https://api.reya.xyz/api/xp/user-game-status/${address}`;
   const res = await fetch(url);
+  if (!res.ok) {
+    throw new Error(await res.text());
+  }
   const json = await res.json();
-  return RenaApiResponseSchema.parse(json);
+  return ReyaApiResponseSchema.parse(json);
 }
